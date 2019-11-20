1a8a5ce769927558656cb8d9e7d0d44823e7e8a6
Julie <juliekivuva@appslab.co.ke>
2019-11-20 13:55:08 +0300

    send arg by bundle fail

--- app/src/main/java/com/julie/learnnavigationcomponents/fragments/LocationFragment.kt
+++ app/src/main/java/com/julie/learnnavigationcomponents/fragments/LocationFragment.kt
@@ -6,21 +6,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 
 import com.julie.learnnavigationcomponents.R
 
-/**
- * A simple [Fragment] subclass.
- */
+
 class LocationFragment : Fragment() {
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_location, container, false)
     }
 
+//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+//        super.onViewCreated(view, savedInstanceState)
+//
+//    }
 
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        val tv = view?.findViewById<TextView>(R.id.textViewAmountl)
+        val amount = arguments?.getString("amount")
+        tv?.text = amount
+    }
 }
--- app/src/main/java/com/julie/learnnavigationcomponents/fragments/MainFragment.kt
+++ app/src/main/java/com/julie/learnnavigationcomponents/fragments/MainFragment.kt
@@ -9,6 +9,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.EditText
 import android.widget.Toast
+import androidx.core.os.bundleOf
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
@@ -17,6 +18,7 @@ import com.julie.learnnavigationcomponents.R
 import kotlinx.android.synthetic.main.fragment_main.*
 
 
+@Suppress("NAME_SHADOWING")
 class MainFragment : Fragment(), View.OnClickListener{
 
     lateinit var navController: NavController
@@ -49,14 +51,25 @@ class MainFragment : Fragment(), View.OnClickListener{
     override fun onClick(v: View?) {
         when(v!!.id){
             R.id.location -> {
-                navController!!.navigate(R.id.action_mainFragment_to_locationFragment)
+                val locationAmount: EditText = view!!.findViewById(R.id.editAmountLocation)
+                if(!TextUtils.isEmpty(locationAmount.text.toString())){
+                    val locationAmount = locationAmount.text.toString().toInt()
+
+                    val bundle = bundleOf("amount" to locationAmount)
+                    v.findNavController().navigate(R.id.action_mainFragment_to_locationFragment, bundle)
+                }else{
+                    Toast.makeText(activity,"Enter amount first", Toast.LENGTH_SHORT).show()
+                }
+
+                //only for navigation
+                //navController!!.navigate(R.id.action_mainFragment_to_locationFragment)
 
             }
 
             R.id.deals -> {
-                var amountTv: EditText = view!!.findViewById(R.id.editTextAmount)
-                if(!TextUtils.isEmpty(amountTv?.text.toString())){
-                    val amount = amountTv?.text.toString().toInt()
+                val amountTv: EditText = view!!.findViewById(R.id.editTextAmount)
+                if(!TextUtils.isEmpty(amountTv.text.toString())){
+                    val amount = amountTv.text.toString().toInt()
                 val action = MainFragmentDirections.actionMainFragmentToDealsFragment().setMyArg(amount)
                 v.findNavController().navigate(action)
                 }else{
